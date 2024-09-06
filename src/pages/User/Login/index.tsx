54994e3a1fa7153f72509b8373dcc73527c811d3
Oliver <olimovier@gmail.com>
2024-09-06 12:44:01 +0800

    Update title and display logo

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -167,8 +167,8 @@ const Login: React.FC = () => {
             minWidth: 280,
             maxWidth: '75vw',
           }}
-          // logo={<img alt="logo" src="/logo.svg" />}
-          title="顺为订单管理系统"
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="订单管理系统"
           // subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
           initialValues={{
             autoLogin: true,
